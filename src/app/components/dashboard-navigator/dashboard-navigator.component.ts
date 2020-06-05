65aaafa0263797f06d9652aad1d44ff4fbedf69c
Anson Foong <ansfoong@gmail.com>
2020-06-04 23:12:02 -0400

    Added functions to navigate dashboard

--- src/app/components/dashboard-navigator/dashboard-navigator.component.ts
+++ src/app/components/dashboard-navigator/dashboard-navigator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-navigator',
@@ -7,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardNavigatorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
 
   }
 
+  navigatePullRequest() {
+    this.router.navigate(['/pullrequest'])
+  }
+
+  navigateApprovedPullRequest() {
+    this.router.navigate(['/'])
+  }
 }
