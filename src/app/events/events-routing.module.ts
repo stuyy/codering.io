dbd335e13f70b50d275e3231feb3b6dd6172d640
Anson Foong <ansfoong@gmail.com>
2020-06-26 07:05:46 -0400

    Added route for /:id/pull-requests to view pull requests for each event.

--- src/app/events/events-routing.module.ts
+++ src/app/events/events-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { EventsPageComponent } from './pages/events-page/events-page.component';
 import { AuthGuard } from '../guards/auth.guard';
 import { LeaderboardsComponent } from './pages/leaderboards/leaderboards.component';
+import { PullRequestComponent } from '../github/pages/pull-request/pull-request.component';
 
 const routes: Routes = [
   { path: '', component: EventsPageComponent },
-  { path: ':id/leaderboards', component: LeaderboardsComponent }
+  { path: ':id/leaderboards', component: LeaderboardsComponent },
+  { path: ':id/pull-requests', component: PullRequestComponent }
 ];
 
 @NgModule({
